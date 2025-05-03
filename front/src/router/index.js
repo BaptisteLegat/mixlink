696009aea23fad8917a7ecd891a3c311a2275ce1
Baptiste Legat <legatbaptiste@gmail.com>
2025-05-03 17:11:01 +0200

    wip contact email

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -2,26 +2,60 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import { useAuthStore } from '@/stores/authStore';
 
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView,
+    },
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('@/views/LoginView.vue'),
+    },
+    {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('@/views/ProfileView.vue'),
+        meta: { requiresAuth: true },
+    },
+    {
+        path: '/terms',
+        name: 'Terms',
+        component: () => import('@/views/TermsView.vue'),
+        meta: {
+            title: 'routes.terms',
+        },
+    },
+    {
+        path: '/privacy',
+        name: 'Privacy',
+        component: () => import('@/views/PrivacyView.vue'),
+        meta: {
+            title: 'routes.privacy',
+        },
+    },
+    {
+        path: '/contact',
+        name: 'Contact',
+        component: () => import('@/views/ContactView.vue'),
+        meta: {
+            title: 'routes.contact',
+        },
+    },
+    {
+        path: '/faq',
+        name: 'FAQ',
+        component: () => import('@/views/FAQView.vue'),
+        meta: {
+            title: 'routes.faq',
+        },
+    },
+];
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: HomeView,
-        },
-        {
-            path: '/login',
-            name: 'login',
-            component: () => import('@/views/LoginView.vue'),
-        },
-        {
-            path: '/profile',
-            name: 'profile',
-            component: () => import('@/views/ProfileView.vue'),
-            meta: { requiresAuth: true },
-        },
-    ],
+    routes,
 });
 
 let userLoaded = false;
